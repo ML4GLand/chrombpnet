db671bbcb13617b4a03a0f159465d950e4d3c6c3
adamklie <aklie@ucsd.edu>
2025-02-25 11:39:19 -0800

    added templates up to contributions, have yet to test

---
+++ scripts/2_bias_pipeline.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+script_path=$1
+num_array=$2
+concurrency=$3
+
+# Slurm settings
+job_name=igvf_sc-islet_10X-Multiome_bias_pipeline
+partition=carter-gpu
+account=carter-gpu
+gpus="a30:1"
+cpus_per_task=4
+mem=128G
+time="14-00:00:00"
+array="1-$num_array%$concurrency"
+output_path="/cellar/users/aklie/data/datasets/igvf_sc-islet_10X-Multiome/bin/slurm_logs/8_sequence_models/chrombpnet/%x.%A.%a.out"
+
+# Cmd
+cmd="sbatch \
+--job-name=$job_name \
+--partition=$partition \
+--account=$account \
+--cpus-per-task=$cpus_per_task \
+--gpus=$gpus \
+--mem=$mem \
+--time=$time \
+--output=$output_path \
+--array=$array \
+$script_path"
+echo -e "Running command:\n$cmd\n"
+eval $cmd
